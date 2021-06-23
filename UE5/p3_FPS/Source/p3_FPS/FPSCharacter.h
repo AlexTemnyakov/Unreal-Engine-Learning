1606afb46c1277bacbdf5e4ada8975277ff6d44d
alextemnyakov <temnyale@fel.cvut.cz>
2021-06-24 00:39:32 +0200

    Working on the player mesh visibility.

--- UE5/p3_FPS/Source/p3_FPS/FPSCharacter.h
+++ UE5/p3_FPS/Source/p3_FPS/FPSCharacter.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "FPSCharacter.generated.h"
 
 UCLASS()
@@ -41,4 +43,12 @@ public:
 	// Clears jump flag when key is released.
 	UFUNCTION()
 	void StopJump();
+
+	// FPS camera.
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* FPSCameraComponent;
+
+	// First-person mesh (arms), visible only to the owning player.
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* FPSMesh;
 };
